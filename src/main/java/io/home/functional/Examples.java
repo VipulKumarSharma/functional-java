9925b27f087ea2fc87eea7ad534cf3c85832684c
Vipul Kumar Sharma <tysonvks@gmail.com>
2020-03-01 22:19:53 +0530

    Track executions

--- src/main/java/io/home/functional/Examples.java
+++ src/main/java/io/home/functional/Examples.java
@@ -1,6 +1,5 @@
 package io.home.functional;
 
-import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
@@ -16,5 +15,29 @@ public class Examples {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("isPrime(7) = "+isPrime(71));
+
+        System.out.println("Tracking execution");
+        int result = IntStream.range(1, 10)
+                .filter(Examples::isGreaterThan3)
+                .filter(Examples::isEven)
+                .map(Examples::doubleIt)
+                .findFirst()
+                .getAsInt();
+        System.out.println(result);
+    }
+
+    static boolean isGreaterThan3(int n) {
+        System.out.println("isGreaterThan3("+n+")");
+        return n > 3;
+    }
+
+    static boolean isEven(int n) {
+        System.out.println("isEven("+n+")");
+        return n % 2 == 0;
+    }
+
+    static int doubleIt(int n) {
+        System.out.println("doubleIt("+n+")");
+        return n * 2;
     }
 }
