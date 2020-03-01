1abf77e5a6ea42791710b7d24b6428551280e109
Vipul Kumar Sharma <tysonvks@gmail.com>
2020-03-01 21:54:09 +0530

    More Examples

--- src/main/java/io/home/functional/Examples.java
+++ src/main/java/io/home/functional/Examples.java
@@ -1,21 +1,20 @@
 package io.home.functional;
 
-import java.util.function.Function;
 import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class Examples {
 
-    static IntPredicate isDivisible(Integer num) {
-        return index -> num % index == 0;
-    }
+    static boolean isPrime(final int number) {
+        Predicate<Integer> isDivisible = divisor -> number % divisor == 0;
 
-    static Function<Integer, Boolean> isPrime
-            = num -> num > 1 && IntStream.range(2, num)
-                                         .noneMatch(isDivisible(num));
+        return number > 1 && IntStream.range(2, number)
+                .noneMatch(isDivisible::test);
+    }
 
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("isPrime.apply(7) = "+isPrime.apply(7));
+        System.out.println("isPrime(7) = "+isPrime(71));
     }
 }
