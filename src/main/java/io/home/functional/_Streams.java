d7eb964e8f3e755b36cd2032255cb86683952de7
Vipul Kumar Sharma <tysonvks@gmail.com>
2020-03-01 21:00:47 +0530

    More Stream functions

--- src/main/java/io/home/functional/_Streams.java
+++ src/main/java/io/home/functional/_Streams.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.home.functional._Streams.Gender.*;
 
@@ -15,6 +17,7 @@ public class _Streams {
     static class Employee {
         private String name;
         private Gender gender;
+        private int age;
     }
 
     enum Gender {
@@ -23,11 +26,11 @@ public class _Streams {
 
     public static void main(String[] args) {
         List<Employee> employees = List.of(
-                new Employee("John", MALE),
-                new Employee("Maria", FEMALE),
-                new Employee("Aisha", FEMALE),
-                new Employee("Alex", MALE),
-                new Employee("Alice", TRANS)
+                new Employee("Adam", MALE, 19),
+                new Employee("Bhawna", FEMALE, 36),
+                new Employee("Casey", FEMALE, 25),
+                new Employee("Daniel", MALE, 27),
+                new Employee("Don", TRANS, 54)
         );
 
         employees.stream()
@@ -50,6 +53,32 @@ public class _Streams {
         boolean noTransGender = employees.stream()
                 .noneMatch(e -> TRANS.equals(e.getGender()));
         System.out.println("No TransGender = "+noTransGender);
+
+        System.out.println("\nSorting employees by (name -> gender)");
+        employees.stream()
+            .sorted(
+                Comparator.comparing(Employee::getName)
+                          .thenComparing(Employee::getGender)
+            )
+            .forEach(System.out::println);
+
+        System.out.println("\nMin age Employee");
+        employees.stream()
+            .min(Comparator.comparing(Employee::getAge))
+            .ifPresent(System.out::println);
+
+        System.out.println("\nMax age Employee");
+        employees.stream()
+                .max(Comparator.comparing(Employee::getAge))
+                .ifPresent(System.out::println);
+
+        System.out.println("\nGroup Employees by Gender");
+        employees.stream()
+                .collect(Collectors.groupingBy(Employee::getGender))
+                .forEach((gender, empList) -> {
+                    System.out.println(gender+" : "+
+                        empList.stream().map(Employee::getName).collect(Collectors.toList()));
+                });
 
     }
 }
